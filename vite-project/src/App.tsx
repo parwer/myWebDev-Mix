be002b7f8f35349f901766e8ca0151a63882bb86
parwer <parwer.ukha@gmail.com>
2022-11-23 20:27:29 +0700

    version 0.1.7 end

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,27 +1,11 @@
 import './App.css'
-import { Private } from './components/auth/Private'
-import { Profile } from './components/auth/Profile'
-import { Counter } from './components/class/Counter'
-import { List } from './components/generics/List'
-import { CustomButton } from './components/html/Button'
-import { Input } from './components/html/Input'
-import { RandomNumber } from './components/restriction/RendomNumber'
-import { Toast } from './components/templateliteral/Toast'
+
 
 function App() {
 
   return (
     <div className="App">
-      <RandomNumber
-        value={10} 
-        isPositive
-      />
-      <Toast position='center'/>
-      <CustomButton variant='primary' onClick={() => {console.log('Clicked')}}>
-        PropTypes
-      </CustomButton>
 
-      <Input type='password'/>
     </div>
   )
 }
